72f3c96df65bafd55793b8be43b08bde9bd9c5cf
Toby Dylan Hocking <tdhock5@gmail.com>
2022-04-14 09:14:58 -0700

    lstm loss compuation

--- demos/2022_04_24.py
+++ demos/2022_04_24.py
@@ -8,8 +8,11 @@ torchtext.datasets.AmazonReviewPolarity
 # consider split='train' as full data set.
 full = torchtext.datasets.IMDB(split='train')
 items_list = list(full)
-items_list[1000]
-items_list[20000]
+def get_size(item):
+    label, text = item
+    print("%s chars, %s words"%(len(text),len(text.split())))
+get_size(items_list[1000])
+get_size(items_list[20000])
 
 word_dict = {}
 split_list = []
@@ -23,13 +26,17 @@ for label, text in items_list:
     item = label, word_list
     split_list.append(item)
 
-[k for k in word_dict.keys()][:5]
+[(k,word_dict[k]) for k in word_dict.keys()][:5]
 max([count for word, count in word_dict.items()])
 # TODO for increased prediction accuracy use a larger dictionary (but
 # that is slower for the demo in class).
 frequent_word_dict = {
-    word:count for word, count in word_dict.items() if count > 99999}
-word_to_ix = {word:ix for ix, word in enumerate(frequent_word_dict)}
+    word:count 
+    for word, count in word_dict.items() 
+    if count > 99999}
+word_to_ix = {
+    word:ix 
+    for ix, word in enumerate(frequent_word_dict)}
 
 feature_list = []
 label_list = []
@@ -44,18 +51,31 @@ for label, word_list in split_list:
 data_dict = {"IMDB":(np.array(feature_list), np.array(label_list))}
 [array.shape for array in data_dict["IMDB"] ]
 
-embed_dim = 4
+embed_dim = 4 # hyper-parameters!!! maybe try 32 or 64.
 hidden_dim = 5
-other_ix = len(frequent_word_dict)
+#not in dictionary of known words
+other_ix = len(frequent_word_dict) 
 vocab_size = other_ix+1 #extra is "other"
 index_list = [word_to_ix.get(w,other_ix) for w in word_list]
 index_tensor = torch.tensor(index_list, dtype=torch.long)
 embed = torch.nn.Embedding(vocab_size, embed_dim)
 embed_out = embed(index_tensor)
+embed_out.shape
 lstm = torch.nn.LSTM(embed_dim, hidden_dim) 
 lstm_in = embed_out.view(len(word_list), 1, -1)
 lstm_in.shape
 embed_out.shape
-lstm_out, hidden = lstm(lstm_in)
+lstm_out, (hidden, cell) = lstm(lstm_in)
 lstm_out.shape
+hidden.shape
+cell.shape
+linear =torch.nn.Linear(hidden_dim, 1)
+pred_score = linear(hidden.reshape(1,hidden_dim))
+label_int = 1 if label is "pos" else 0
+label_tensor = torch.tensor([float(label_int)])
+loss_fun = torch.nn.BCEWithLogitsLoss()
+loss_value = loss_fun(pred_score.reshape(1), label_tensor)
 ## TODO connect to label.
+loss_value.backward()
+model = your_NN_class()
+optimizer = torch.optim.SGD(model.parameters())
